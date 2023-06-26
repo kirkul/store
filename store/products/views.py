5f4dcb7d37c2a7f745b1d344764c579bdfe6992c
Kirill Kulev <kirkulev101@mail.ru>
2023-06-26 23:06:45 +0300

    created filtering by categories

--- store/products/views.py
+++ store/products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from products.models import Product,ProductCategory,Basket
 
+from django.contrib.auth.decorators import login_required
+
 from users.models import User
 # Create your views here.
 
@@ -8,15 +10,16 @@ def index(request):
     context = {'title':'Store'}
     return render(request,'products/index.html', context)
 
-def products(request):
+def products(request, category_id=None):
     context={
         'title':'Store - Каталог',
-        'products': Product.objects.all(),
         'categories':ProductCategory.objects.all(),
+        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
     }
+
     return render(request, 'products/products.html', context)
 
-
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets= Basket.objects.filter(user=request.user, product=product)
@@ -29,6 +32,7 @@ def basket_add(request, product_id):
         basket.save()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
--- store/users/views.py
+++ store/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 from users.forms import UserLoginForm,UserRegistrationForm,UserProfileForm
+from django.contrib.auth.decorators import login_required
 
 from products.models import Basket
 
@@ -34,6 +35,7 @@ def registration(request):
     context = {'form': form}
     return render(request, 'users/registration.html', context)
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form=UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
