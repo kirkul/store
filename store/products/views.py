cd84a79e65db3f1f2a7de868be422a81adf2f60c
Kirill Kulev <kirkulev101@mail.ru>
2023-06-27 23:23:03 +0300

    create index view

--- store/products/views.py
+++ store/products/views.py
@@ -1,15 +1,16 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, HttpResponseRedirect
 from products.models import Product,ProductCategory,Basket
+from django.views.generic.base import TemplateView
 
 from django.contrib.auth.decorators import login_required
 
-from users.models import User
-# Create your views here.
-
-def index(request):
-    context = {'title':'Store'}
-    return render(request,'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(IndexView,self).get_context_data()
+        context['title'] = 'Store'
+        return context
 
 def products(request, category_id=None, page_number=1):
     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
