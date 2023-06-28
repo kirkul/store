d39b5187663152c86d25374727a45545ae22e469
Kirill Kulev <kirkulev101@mail.ru>
2023-06-28 23:19:30 +0300

    change product_list_view

--- store/products/views.py
+++ store/products/views.py
@@ -1,8 +1,7 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from products.models import Product,ProductCategory,Basket
 from django.views.generic.base import TemplateView
-
+from django.views.generic.list import ListView
 from django.contrib.auth.decorators import login_required
 
 class IndexView(TemplateView):
@@ -12,18 +11,20 @@ class IndexView(TemplateView):
         context['title'] = 'Store'
         return context
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    per_page=3
-    paginator = Paginator(products,per_page)
-    products_paginator = paginator.page(page_number)
-    context={
-        'title':'Store - Каталог',
-        'categories':ProductCategory.objects.all(),
-        'products': products_paginator
-    }
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
-    return render(request, 'products/products.html', context)
+    def get_queryset(self):
+        queryset = super(ProductsListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context =  super(ProductsListView, self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
 @login_required
 def basket_add(request, product_id):
