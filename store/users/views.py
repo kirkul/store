86cc24b2378f3a43cbac5a658aa8cbb2bc534f00
Kirill Kulev <kirkulev101@mail.ru>
2023-07-06 00:00:56 +0300

    create EmailVerification

--- store/users/views.py
+++ store/users/views.py
@@ -1,10 +1,12 @@
-from django.urls import reverse_lazy
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic.base import TemplateView
 from users.forms import UserLoginForm,UserRegistrationForm,UserProfileForm
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 from products.models import Basket
-from users.models import User
+from users.models import User,EmailVerification
 from common.views import TitleMixin
 
 class UserLoginView(TitleMixin, LoginView):
@@ -33,3 +35,17 @@ class UserProfileView(TitleMixin, UpdateView):
         return context
 
 
+class EmailVerificationView(TitleMixin, TemplateView):
+    title = 'Store - Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
+
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verifications = EmailVerification.objects.filter(user = user, code = code)
+        if email_verifications.exists():
+            user.is_verified_email=True
+            user.save()
+            return super(EmailVerificationView, self).get(request, *args, **kwargs)
+        else:
+            HttpResponseRedirect(reverse('index'))
