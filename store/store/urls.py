17cbe5f87bf957f04dc89a7c3f71e853b67a4e36
Kirill Kulev <kirkulev101@mail.ru>
2023-07-17 23:43:11 +0300

    added webhook

--- store/store/urls.py
+++ store/store/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from orders.views import stripe_webhook_view
 from products.views import IndexView
 
 urlpatterns = [
@@ -27,6 +27,8 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('accounts/', include('allauth.urls')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('webhook/stripe/', stripe_webhook_view, name='stripe_webhook'),
+
 ]
 
 if settings.DEBUG:
